9560c04342f061baed75d20b88894385cfdc9e46
SoulNinja-dev <aadhi123awesome@gmail.com>
2022-08-19 23:57:11 +0530

    custom value

--- contracts/City.sol
+++ contracts/City.sol
@@ -6,49 +6,76 @@ import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/security/PullPayment.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
+import "hardhat/console.sol";
 
 contract City is ERC721, PullPayment, Ownable {
-  using Counters for Counters.Counter;
+    using Counters for Counters.Counter;
 
-  // Constants
-  uint256 public constant TOTAL_SUPPLY = 10000;
-  uint256 public constant MINT_PRICE = 0.08 ether;
+    // Constants
+    uint256 public constant TOTAL_SUPPLY = 10000;
+    // uint256 public constant MINT_PRICE = 0.08 ether;
 
-  Counters.Counter private currentTokenId;
+    Counters.Counter private currentTokenId;
 
-  /// @dev Base token URI used as a prefix by tokenURI().
-  string public baseTokenURI;
+    /// @dev Base token URI used as a prefix by tokenURI().
+    string public baseTokenURI;
 
-  constructor() ERC721("City", "CTY") {
-    baseTokenURI = "https://bafybeihqgz7fqruizwhsrxlxwcvsmankd7wth5lr5d2zfzm3mmcsj7wvvu.ipfs.nftstorage.link/metadata/";
-  }
+    /// @dev store the value of the toekn with tokenID
+    mapping(uint256 => uint256) public tokenValue;
 
-  // @dev mint token from next tokenId
-  // @dev can only mint once in 24 hours
-  // @dev accept value from user instead of using a mint price
-  function mintTo(address recipient) public payable returns (uint256) {
-    uint256 tokenId = currentTokenId.current();
-    require(tokenId < TOTAL_SUPPLY, "Max supply reached");
-    require(msg.value == MINT_PRICE, "Transaction value did not equal the mint price");
+    constructor() ERC721("City", "CTY") {
+        baseTokenURI = "https://bafybeihqgz7fqruizwhsrxlxwcvsmankd7wth5lr5d2zfzm3mmcsj7wvvu.ipfs.nftstorage.link/metadata/";
+    }
 
-    currentTokenId.increment();
-    uint256 newItemId = currentTokenId.current();
-    _safeMint(recipient, newItemId);
-    return newItemId;
-  }
+    // @dev mint token from next tokenId
+    // @dev can only mint once in 24 hours
+    // @dev accept value from user instead of using a mint price
+    function mintTo(address recipient, uint256 value)
+        public
+        payable
+        returns (uint256)
+    {
+        uint256 tokenId = currentTokenId.current();
+        require(tokenId < TOTAL_SUPPLY, "Max supply reached");
+        require(
+            msg.value == value,
+            "Transaction value did not equal the value given"
+        );
 
-  /// @dev Returns an URI for a given token ID
-  function _baseURI() internal view virtual override returns (string memory) {
-    return baseTokenURI;
-  }
+        currentTokenId.increment();
+        uint256 newItemId = currentTokenId.current();
+        _safeMint(recipient, newItemId);
+        tokenValue[newItemId] = value;
+        console.log("newItemId: %s", newItemId);
+        return newItemId;
+    }
 
-  /// @dev Sets the base token URI prefix.
-  function setBaseTokenURI(string memory _baseTokenURI) public onlyOwner {
-    baseTokenURI = _baseTokenURI;
-  }
+    /// @dev Returns an URI for a given token ID
+    function _baseURI() internal view virtual override returns (string memory) {
+        return baseTokenURI;
+    }
 
-  /// @dev Overridden in order to make it an onlyOwner function
-  function withdrawPayments(address payable payee) public override onlyOwner virtual {
-      super.withdrawPayments(payee);
-  }
+    /// @dev Sets the base token URI prefix.
+    function setBaseTokenURI(string memory _baseTokenURI) public onlyOwner {
+        baseTokenURI = _baseTokenURI;
+    }
+
+    /// @dev Overridden in order to make it an onlyOwner function
+    function withdrawPayments(address payable payee)
+        public
+        virtual
+        override
+        onlyOwner
+    {
+        super.withdrawPayments(payee);
+    }
+
+    /// @dev Get the minted token values
+    function getTokenValue(uint256 tokenId)
+        public
+        view
+        returns (uint256)
+    {
+        return tokenValue[tokenId];
+    }
 }
--- scripts/mint.js
+++ scripts/mint.js
@@ -1,16 +1,37 @@
+// 0xe7dac11acd0a04b6c8daa37d3d6223ba301d418a
+const { Contract } = require("ethers");
 const { ethers } = require("hardhat");
+const CityAbi = require("./City.json");
 
 const main = async () => {
   const [owner] = await hre.ethers.getSigners();
-  const cityFactory = await hre.ethers.getContractFactory("City");
-  const cityContract = await cityFactory.deploy();
+  // const cityFactory = await hre.ethers.getContractFactory("City");
+  // const cityContract = await cityFactory.deploy();
+  const abi = CityAbi.abi;
+  const cityContract = new Contract(
+    "0xe7dac11acd0a04b6c8daa37d3d6223ba301d418a",
+    abi,
+    owner
+  );
 
   console.log("Contract deployed to:", cityContract.address);
-  const city = await cityContract.mintTo(owner.address, {
-    gasLimit: 500000,
-    value: ethers.utils.parseEther("0.08"),
-  });
+  const city = await cityContract.mintTo(
+    owner.address,
+    ethers.utils.parseEther("0.03"),
+    {
+      gasLimit: 500000,
+      value: ethers.utils.parseEther("0.03"),
+    }
+  );
   console.log(`City minted to ${owner.address} @ ${city.hash}`);
+
+  // get value returned by cityContract.mintTo
+  const cityItemId = await city.wait();
+  console.log(cityItemId);
+
+  console.log(city);
+  const value = await cityContract.getTokenValue(1);
+  console.log(`token value: ${value}`);
 };
 
 const runMain = async () => {
